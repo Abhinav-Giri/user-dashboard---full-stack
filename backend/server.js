bb20fc810950386b8a53223314c537eb0ff241e5
Abhinav-Giri <iamabhinavgiri@gmail.com>
2026-03-26 16:45:54 +0530

    modified URL as per deployment changes

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,16 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // Local Deployment
+      "https://user-dashboard-gamma-ecru.vercel.app", // Vercel URL
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
